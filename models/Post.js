dfb8eadc68f0aa4bd0e507f6ca2b2df72b4fd9bf
properfunction <111qazxccv@gmail.com>
2025-03-11 16:16:54 +1000

    Set up schemas and session management with MongoDB optimisation

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    cloudinaryId: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    caption: {
+        type: String, 
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model("Post", PostSchema);
--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 // Core modules
 const express = require('express');
+const session = require('express-session')
 const app = express(); // Initialize express app
 
 // Third-party modules
-require("dotenv").config({ path: "./config/.env" }) // Load environment variables and instantiate it with the config() written last
-// const mongoose = require('mongoose') // *** Already imported in database.js, don't think it's necessary here ***
+require("dotenv").config({ path: "./config/.env" }) // Load environment variables
+const mongoose = require('mongoose')
+const MongoStore = require('connect-mongo')
 
 // Local modules
 const connectDB = require('./config/database.js')
 
-// Middleware
+// Connect to Database 
+connectDB()
+
+// Static folder
+app.use(express.static("public"))
+
+// Body Parsing
 app.use(express.json()) // Parses JSON bodies
 app.use(express.urlencoded({ extended: true })) // Parses url-encoded bodies from forms
 
-connectDB() // Call the function to connect to DB after all the imports are done
+// Setup Sessions - stored in MongoDB
+app.use(
+    session({
+      secret: "keyboard cat", // Change this to a secure string
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        // mongoUrl: process.env.DB_STRING,
+        client: mongoose.connection.client, // Reuse the existing Mongoose client connection to avoid creating a separate connection for session storage
+        ttl: 14 * 24 * 60 * 60, // Optional: time to live (in seconds). After that, session will expire and user will need to login again
+      }),
+    })
+  );
+  
 
+// Setup Routes that server is listening for
 app.get('/', (req, res) => {
     res.send('Hello wd')
 })
 
 // Server running
 app.listen(process.env.PORT, () => { // Use PORT from process.env
-    console.log('server running')
+    console.log('Server is running, you better go catch it!')
 })
