5c7545a21bba1fdc698fbb9f55aecbbd4d4fb41d
properfunction <111qazxccv@gmail.com>
2025-03-12 22:27:41 +1000

    Add file upload to cloudinary cloud storage and handle file upload to server using multer

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('../middleware/multer')
+const postController = require('../controllers/post')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.post('/createPost', upload.single("file"), postController.createPost) // The upload.single("file") specifies to expect a single file to be uploaded and "file" is the name of the input field in the HTML form
+
+
+
+
 
 module.exports = router;
